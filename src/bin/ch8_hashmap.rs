59da3acb953c5db610ba4b2c94d8da405cacc4f0
Great <ezennagreat@gmail.com>
2026-07-19 02:39:26 +0100

    feat: concluded the row_to_map function, can now serialize a Row struct into a Rust HashMap

--- src/bin/ch8_hashmap.rs
+++ src/bin/ch8_hashmap.rs
@@ -1,18 +1,36 @@
 use serde_json;
+use std::collections::HashMap;
+
+struct Row {
+    id: u32,
+    name: String,
+    age: u8,
+}
+
+fn row_to_map(row: &Row) -> HashMap<String, String> {
+    let map = HashMap::new();
+
+    map.insert("id".to_string(), row.id.to_string());
+    map.insert("name".to_string(), name);
+    map.insert("age".to_string(), row.age.to_string());
+
+    map
+}
+
+//fn map_to_json(map: &HashMap<String, String>) -> String {
+//    todo!()
+//}
 
 fn main() {
-    let data = r#"
-        {
-            "name": "John Doe",
-            "age": 43,
-            "phones": [
-                "+44 1234567",
-                "+44 2468902"
-            ]
-        }
-        "#;
+    let row = Row {
+        id: 1,
+        name: String::from("Great"),
+        age: 18,
+    };
 
-    let v: serde_json::Value = serde_json::from_str(data).expect("REASON");
+    let map = row_to_map(&row);
+    // let json = map_to_json(&map);
 
-    println!("v: {v}")
+    println!("map: {:?}", map);
+    // println!("{}", json);
 }
