2bb980b283f25f21889b158c8aa2ada3a3d0e345
Great <ezennagreat@gmail.com>
2026-07-20 23:14:17 +0100

    fix: updated to use serde's Serialize, and Deserialize traits, to automatically reconstruct the JSON fields to a Row struct and vice versa

--- src/bin/ch9_errors.rs
+++ src/bin/ch9_errors.rs
@@ -1,30 +1,20 @@
 // will eventually get better, as i move up chapters in the Book
 
-use serde_json;
-use std::collections::HashMap;
+use serde::{self, Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Row {
     id: u32,
     name: String,
     age: u8,
 }
 
-fn row_to_map(row: &Row) -> HashMap<String, String> {
-    let mut map = HashMap::new();
-
-    map.insert("id".to_string(), row.id.to_string());
-    map.insert("name".to_string(), row.name.clone());
-    map.insert("age".to_string(), row.age.to_string());
-
-    map
-}
-
-fn map_to_json(map: &HashMap<String, String>) -> Result<String, serde_json::Error> {
-    let json = serde_json::to_string(&map)?;
+fn map_to_json(row: &Row) -> Result<String, serde_json::Error> {
+    let json = serde_json::to_string(&row)?;
     Ok(json)
 }
 
-fn json_to_map(json_str: &str) -> Result<HashMap<String, String>, serde_json::Error> {
+fn json_to_map(json_str: &str) -> Result<Row, serde_json::Error> {
     let map = serde_json::from_str(&json_str)?;
     Ok(map)
 }
@@ -36,11 +26,9 @@ fn main() {
         age: 18,
     };
 
-    let map = row_to_map(&row);
-
-    let json = map_to_json(&map).unwrap();
+    let json = map_to_json(&row).unwrap();
     println!("{}", json);
 
-    let back: HashMap<String, String> = json_to_map(&json).unwrap();
+    let back = json_to_map(&json).unwrap();
     println!("{:?}", back);
 }
